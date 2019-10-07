45c13b4e336cd1f259bb65c4c42faf9984a28fca
Kai Blin <kblin@biosustain.dtu.dk>
2019-10-07 14:20:18 +0200

    chore: Add sequence download script
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
--
+MIBiG curation data in JSON format
+==================================
+
+This repository tracks the current [MIBiG] annotations in JSON format.
+
+Contents
+--------
+
+The current live datasets for [MIBiG] live in the `data` directory.
+Datasets that have been retired because of duplications, or annotation and/or
+sequence data quality issues can be found in the `retired` directory.
+To download the sequences described by the JSON files, there is a script in the
+`scripts` directory. The `genbanks` directory contains a handful of sequence
+files that are not available from NCBI's GenBank/RefSeq databases.
+
+
+[MIBiG]: https://mibig.secondarymetabolites.org/
---
+++ scripts/download_genbanks.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# Download GenBank files for MIBiG entries
+# Requires:
+# * jq
+# * ncbi-acc-download
+# * sed
+
+
+set -o errexit
+set -o pipefail
+set -o errexit
+
+DEFAULT_JSONS_DIR="$(dirname $(dirname $0))/data"
+DEFAULT_GENBANKS_DIR="$(dirname $(dirname $0))/genbanks"
+
+usage() {
+    echo "Download the GenBank files related to the MIBiG entries"
+    echo "Usage: $0 [-j|--jsons dir-with-mibig-jsons] [-g|--genbanks dir-to-hold-gbk-files]"
+    echo "-j <path> | --jsons <path>       Path to directory containing MIBiG JSON files (default: ${DEFAULT_JSONS_DIR})"
+    echo "-g <path> | --genbanks <path>    Path to directory to hold the downloaded GenBank files (default: ${DEFAULT_GENBANKS_DIR})"
+}
+
+download() {
+    ACC=$1
+    GENBANKS_DIR=$2
+    pushd ${GENBANKS_DIR} > /dev/null
+    if [ ! -e "${ACC}.gbk" ]; then
+        ncbi-acc-download --recursive $ACC
+    fi
+    popd > /dev/null
+}
+
+getAcc() {
+    JSON_FILE=$1
+    jq '.cluster.loci.accession' $JSON_FILE | sed -e 's/"//g'
+}
+
+run() {
+    JSONS_DIR=$1
+    GENBANKS_DIR=$2
+    for JSON_FILE in ${JSONS_DIR}/*.json; do
+        ACC=$(getAcc ${JSON_FILE})
+        BGC_ID=$(basename ${JSON_FILE/\.json/})
+        echo "$BGC_ID: $ACC" 1>&2
+        download $ACC ${GENBANKS_DIR}
+    done
+}
+
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -h|--help)
+            usage
+            exit 0
+            ;;
+        -j|--jsons)
+            JSONS_DIR="$2"
+            shift; shift
+            ;;
+        -g|--genbanks)
+            GENBANKS_DIR="$2"
+            shift; shift
+            ;;
+        *)
+            usage
+            exit 2
+            ;;
+    esac
+done
+
+: ${JSONS_DIR:=${DEFAULT_JSONS_DIR}}
+: ${GENBANKS_DIR:=${DEFAULT_GENBANKS_DIR}}
+
+if [[ ! -d ${GENBANKS_DIR} ]]; then
+    mkdir -p ${GENBANKS_DIR}
+fi
+
+run ${JSONS_DIR} ${GENBANKS_DIR}
