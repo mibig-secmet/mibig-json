6e490194f6a2a916fc77d144d0065e82f1fd1a3b
Kai Blin <kblin@biosustain.dtu.dk>
2021-10-20 20:23:38 +0200

    scripts: Add a script to easily add things to the changelog
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

---
+++ scripts/add_changelog.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+from typing import Any, Dict, List
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-r", "--release", default="2.1",
+                        help="Set the release version of the changelog entry to add (default: %(default)s).")
+    parser.add_argument("-c", "--comment", action="append",
+                        help="Comment to add to changelog entry (can be used multiple times)")
+    parser.add_argument("-C", "--contributor", action="append",
+                        help="Contributor ID of a contributor to this change (can be used multiple times)")
+    parser.add_argument("-a", "--amend", action="store_true", default=False,
+                        help="Fix the last changelog entry instead of adding a new entry")
+    parser.add_argument("json_file", help="Filename of json file to add changelog entry to.")
+    args = parser.parse_args()
+    run(args)
+
+
+def run(args: argparse.Namespace) -> None:
+    with open(args.json_file, 'r', encoding="utf-8") as handle:
+        record = json.load(handle)
+
+    if args.amend:
+        amend_changelog(record, args.release, args.comment, args.contributor)
+    else:
+        add_changelog(record, args.release, args.comment, args.contributor)
+
+    with open(args.json_file, 'w', encoding="utf-8") as handle:
+        json.dump(record, handle, indent=4, separators=(',', ': '), sort_keys=True, ensure_ascii=False)
+
+
+def add_changelog(record: Dict[str, Any], release: str, comments: List[str], contributors: List[str]) -> None:
+
+    if not contributors:
+        contributors = ["AAAAAAAAAAAAAAAAAAAAAAAA"]
+
+    new_entry = {
+        "comments": comments,
+        "contributors": contributors,
+        "version": release,
+    }
+
+    record["changelog"].append(new_entry)
+
+
+def amend_changelog(record: Dict[str, Any], release: str, comments: List[str], contributors: List[str]) -> None:
+    entry = record["changelog"][-1]
+    entry["version"] = release
+    if comments:
+        if "comments" not in entry or not entry["comments"]:
+            entry["comments"] = comments
+        else:
+            entry["comments"].extend(comments)
+
+    if contributors:
+        if "contributors" not in entry or not entry["contributors"]:
+            entry["contributors"] = contributors
+        else:
+            entry["contributors"].extend(contributors)
+
+
+if __name__ == "__main__":
+    main()
